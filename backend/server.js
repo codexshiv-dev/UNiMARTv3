a529f103ece7e20c5908bc2a628397239ff8fb81
Gupta shiv <codexshiv.dev@gmail.com>
2026-03-25 13:20:48 +0530

    small change server

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 
 const app = express();
 
-// ✅ ADD THIS LINE (VERY IMPORTANT)
+//ADD THIS LINE (VERY IMPORTANT)
 app.use(cors());
 
 // Middleware
@@ -25,7 +25,7 @@ const productRoutes = require("./routes/productRoutes");
 app.use("/api/products", productRoutes);
 
 // Server start
-const PORT = 5000;
+const PORT =  process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
